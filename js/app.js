90ed3fa270e4a7e5867d6597150bc1377ab93fdf
naitRAM <rmansieh@gmail.com>
2017-07-19 22:23:52 -0400

    adding jQuery code to app.js, stressing out over bug

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 
+$("button").click(function(e) {
+	e.preventDefault();
+	var value = $(".input-box").val();
+	div = "<div class='item-box'> <h3 class='item-text'>" + value + "</h3> <input type='checkbox'> <i class='fa fa-trash fa-2x bin-button' aria-hidden='true'></i> </div>";
+	$(".main").append(div);
+	$(".bin-button").click(function() {
+	$(this).parent("div.item-box").remove();
+})
+	$("[type=checkbox]").click(function() {
+	$(this).siblings(".item-text").toggleClass("strikethrough");
+})
+});
+
+
+
